4c749d01ea8021284547a014e206389eeb976e22
Rimmer den Hoed <rimmer@denhoed.coffee>
2026-04-28 20:33:03 +0200

    Update API key and push to GitHub

--- api/contact.ts
+++ api/contact.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_eiv7MH6X_BCmhhu1mgngLxezMCZFNqvjs');
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
